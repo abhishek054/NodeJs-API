45ce5f3c9c8c796058d430cfe14bc637a79bd61a
Abhishek Bharti <abhishek054bharti@google.com>
2018-09-04 22:29:35 +0530

    4 sept night

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
 var express = require("express");
 var app = express();
 var fs = require("fs");
-
-app.get("/", function(req, res, next) {
-  res.send(
-    "Get request is working  " +
-      fs.readFileSync("../data/file.txt", (err, data) => {
-        console.log(JSON.parse(data));
-      })
-  );
+app.get("/get", function(req, res) {
+  // console.log(req);
+  // fs.writeFileSync("../data/temp.txt", JSON.parse(req));
+  res.send(fs.readFileSync("../data/file.json", "utf8"));
+  // fs.readFile("../data/file.json", "utf8", (err, data) => {
+  //   // var d = JSON.parse(data);
+  //   console.log(data);
+  //   res.send("Get request data : " + data);
+  // });
 });
 
-const port = process.env.port || 3000;
+console.log("running");
+// const port = process.env.port || 3000;
 
-app.listen(port, () => {
-  console.log(`${port} working`);
+app.listen(4200, () => {
+  console.log(`working on 4200`);
 });
 
-app.post();
+app.post("/post", (req, res) => {
+  // fs.writeFile("../data/temp.json", JSON.parse(p));
+  res.send(req.params);
+});
 
 // app.put();
 
