641605f736a3c2a998207f7be0662ddcfd37fe3b
Abhishek Bharti <abhishek054bharti@google.com>
2018-09-08 01:08:44 +0530

    Only delete button left

--- src/react_app_API.js
+++ src/react_app_API.js
@@ -8,10 +8,10 @@ const cors = require("cors");
 app.listen(10000, () => {
   console.log("Working at port 10000");
 });
+
 app.use(cors());
 
 //SignUp
-
 app.post("/signUp", jsonParser, (req, res) => {
   fs.readFile("../data/react_app_data.json", "utf8", (err, data) => {
     data = JSON.parse(data);
@@ -25,7 +25,6 @@ app.post("/signUp", jsonParser, (req, res) => {
 });
 
 //SignIn
-
 app.post("/signIn", jsonParser, (req, res) => {
   fs.readFile("../data/react_app_data.json", "utf8", (err, data) => {
     data = JSON.parse(data);
@@ -50,4 +49,60 @@ app.post("/signIn", jsonParser, (req, res) => {
   });
 });
 
+//Add notes
+app.post("/addNotes", jsonParser, verifyToken, (req, res) => {
+  jwt.verify(req.token, "secretKey", (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      var user;
+      fs.readFile("../data/react_app_data.json", "utf8", (err, data) => {
+        data = JSON.parse(data);
+        data.find(x => x.email == authData.email).notes.push(req.body);
+        fs.writeFileSync(
+          "../data/react_app_data.json",
+          JSON.stringify(data, null, 2)
+        );
+      });
+    }
+  });
+  res.send("Data Saved");
+});
+
+//Delete notes
+
 //Send Notes Data
+app.get("/getInfo", verifyToken, (req, res) => {
+  jwt.verify(req.token, "secretKey", (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      var found = false;
+      var emailId = authData.email;
+      fs.readFile("../data/react_app_data.json", "utf8", (err, data) => {
+        data = JSON.parse(data);
+        data.map(x => {
+          if (x.email == emailId) {
+            res.send(x.notes);
+            found = true;
+          }
+        });
+        if (!found) {
+          res.send("Data not found");
+        }
+      });
+    }
+  });
+});
+
+//verifyToken
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+  if (bearerHeader) {
+    let bearer = bearerHeader.split(" ")[1];
+    req.token = bearer;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
