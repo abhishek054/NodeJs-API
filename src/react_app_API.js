b2dedf9e4a19d3bfb2e1560950bae63c40ab4136
Abhishek Bharti <abhishek054bharti@google.com>
2018-09-08 16:03:24 +0530

    Fully functional

--- src/react_app_API.js
+++ src/react_app_API.js
@@ -21,28 +21,36 @@ app.post("/signUp", jsonParser, (req, res) => {
       JSON.stringify(data, null, 2)
     );
   });
-  res.send("Saved Successfully");
+  res.send("Account Created Successfully");
 });
 
 //SignIn
 app.post("/signIn", jsonParser, (req, res) => {
   fs.readFile("../data/react_app_data.json", "utf8", (err, data) => {
     data = JSON.parse(data);
-    var userData;
+    var userData = {};
     var tokenUserData;
     var c = false;
-    data.map(x => {
-      if (x.email == req.body["email"] && x.password == req.body["password"]) {
-        c = true;
-        userData = { name: x.name };
-        tokenUserData = { name: x.name, email: x.email };
-      }
-    });
-    if (!c) {
+    var userName, userMail;
+    userName = data.find(
+      x => x.email == req.body["email"] && x.password == req.body["password"]
+    ).name;
+    userMail = data.find(
+      x => x.email == req.body["email"] && x.password == req.body["password"]
+    ).email;
+    userData = { name: userName };
+    tokenUserData = { name: userName, email: userMail };
+    // console.log(userName + userMail);
+    if ((userData.name = null)) {
       res.send("Not Found");
     } else {
       jwt.sign(tokenUserData, "secretKey", (err, token) => {
-        userData["token"] = token;
+        userData = {
+          name: userName,
+          token: token
+        };
+        // userData["token"] = token;
+        // console.log(userData);
         res.send(userData);
       });
     }
@@ -70,6 +78,31 @@ app.post("/addNotes", jsonParser, verifyToken, (req, res) => {
 });
 
 //Delete notes
+// app.delete("/deleteNote", verifyToken, (req, res) => {
+//   jwt.verify(req.token, "secretKey", (err, authData) => {
+//     console.log(req.body);
+//     res.send("Deleted");
+//   });
+// });
+
+app.post("/deleteNote", jsonParser, verifyToken, (req, res) => {
+  jwt.verify(req.token, "secretKey", (err, authData) => {
+    fs.readFile("../data/react_app_data.json", "utf8", (err, DATA) => {
+      DATA = JSON.parse(DATA);
+      var d = DATA.find(x => x.email == authData.email);
+      d.notes.splice(d.notes.indexOf(req.body), 1);
+      // d.notes.splice(d.notes.indexOf(req.body), 1);
+      // DATA.find(x => x.email == authData.email).notes = newNotes;
+      // console.log(DATA);
+      fs.writeFileSync(
+        "../data/react_app_data.json",
+        JSON.stringify(DATA, null, 2)
+      );
+    });
+    console.log(req.body);
+    res.send("Deleted");
+  });
+});
 
 //Send Notes Data
 app.get("/getInfo", verifyToken, (req, res) => {
